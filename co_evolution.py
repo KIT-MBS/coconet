1849620cf74adf6b3da5d1203499d09fd1a162b1
Mehari <mbzerihun@gmail.com>
2020-06-23 17:55:01 +0200

    added co_evolution.py

---
+++ co_evolution.py
@@ -0,0 +1,9 @@
+import logging 
+
+"""This module performs RNA nucleotide-nucleotide coevolutionary computation  
+using direc coupling analyis (DCA). 
+
+Author:  Mehari B. Zerihun
+"""
+
+logger = logging.getLogger(__name__)
