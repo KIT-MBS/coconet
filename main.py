0d994bb6ed4f36e2a37d0c307f41376155c39718
Mehari <mbzerihun@gmail.com>
2020-06-23 18:00:34 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging 
+
+"""Main module for coconet 
+
+Author: Mehari B. Zerihun 
+"""
+
+
+logger = logging.getLogger(__name__)
+
+
+def execute_from_command_line():
+    """
+    """
+    raise NotImplementedError
+
+
+
+if __name__ == '__main__':
+    execute_from_command_line()
