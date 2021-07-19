2062b6861c5a07a8103415505b6c40ce09c8a761
Mehari <mbzerihun@gmail.com>
2021-07-19 17:17:51 +0200

    added --all_filters optional argument

--- coconet/cmdargs.py
+++ coconet/cmdargs.py
@@ -26,18 +26,21 @@ class CmdArgs:
 
     # plmDCA specific args
     on_plm_optional = '--on_plm'
-    on_plm_optional_help = """Do convolution on top of plmDCA"""
+    on_plm_optional_help = """Do convolution on top of plmDCA."""
     
     max_iterations_optional = '--max_iterations'
     max_iterations_help = """Number of iterations for gradient decent 
     for negative pseudolikelihood minimization.
     """
     num_threads_optional = '--num_threads'
-    num_threads_help = "Number of threads from plmDCA computation"
+    num_threads_help = "Number of threads from plmDCA computation."
 
     num_trials_optional = '--num_trials'
     num_trials_optional_help = '''Number of repitions for cross validation. Each time a repitition is done, 
-        the RNA data is shuffiled to form new sets of RNAs in each fold'''
+        the RNA data is shuffiled to form new sets of RNAs in each fold.'''
+    for_all_matrices_optional = '--all_filters'
+    for_all_matrices_optional_help = """Train CoCoNet for all filter matrix sizes in one run.
+    """
 
 if __name__ == '__main__':
     print('You are running module {}'.format(__file__))
